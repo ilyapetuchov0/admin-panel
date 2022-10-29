f7a2db945512abc7874a61e045e49108a3459679
ilyapetuchov0 <ilyapetuchov0@gmail.com>
2022-10-29 23:09:19 +0300

    Added sidebar

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,11 @@
+import "./list.scss"
+
+const List = () => {
+    return (
+        <div className="home">
+            <h1 className="title">Googolplex</h1>
+        </div>
+    )
+}
+
+export default List

2e59f30a4ec893157297eea9669d33f08f49ff51
ilyapetuchov0 <ilyapetuchov0@gmail.com>
2022-10-30 00:01:23 +0300

    Added navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,48 @@
 import "./navbar.scss"
 
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+
 const Navbar = () => {
     return (
         <div className="navbar">
-            Navbar
+            <div className="wrapper">
+                <div className="search">
+                    <input type="text" placeholder="Search..." />
+                    <SearchIcon className="icon" />
+                </div>
+                <div className="items">
+                    <div className="item">
+                        <LanguageIcon className="icon" />
+                        English
+                    </div>
+                    <div className="item">
+                        <DarkModeOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <FullscreenExitOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <NotificationsNoneOutlinedIcon className="icon" />
+                        <div className="counter">1</div>
+                    </div>
+                    <div className="item">
+                        <ChatBubbleOutlineIcon className="icon" />
+                        <div className="counter">2</div>
+                    </div>
+                    <div className="item">
+                        <ListOutlinedIcon className="icon" />
+                    </div>
+                    <div className="item">
+                        <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2680&q=80" alt="User's avatar" className="avatar" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
